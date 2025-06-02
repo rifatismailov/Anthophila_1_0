cea15a7f37e42dd3385b06101412776a27ec5a1b
Rifat Ismailov <refaazer@gmail.com>
2025-06-02 13:38:17 +0300

    Initial commit

--- фш/test/management/message.go
+++
@@ -1,8 +0,0 @@
-package management
-
-// Message — загальна структура для обміну повідомленнями між клієнтами через WebSocket.
-type Message struct {
-	SClient string `json:"sClient"`
-	RClient string `json:"rClient"`
-	Message string `json:"message"`
-}
--- фш/test/management/terminalinterface.go
+++
@@ -1,8 +0,0 @@
-package management
-
-// TerminalInterface визначає загальні методи для роботи з терміналом
-type TerminalInterface interface {
-	Start() error
-	Stop()
-	SendCommand(command string)
-}
